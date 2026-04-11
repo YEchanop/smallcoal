56c877653caef3202e0f30304735498328075b92
YEchanop <1474891484@qq.com>
2026-04-11 09:32:56 +0800

    Add files via upload

---
+++ src/main/java/com/yechandd/smallcoal/Config.java
@@ -0,0 +1,7 @@
+package com.yechandd.smallcoal;
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+public class Config {
+    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+    static final ModConfigSpec SPEC = BUILDER.build();
+    }
